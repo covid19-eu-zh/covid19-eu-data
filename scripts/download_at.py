47f10ed91f5efb1ed2984b0823de996f8b84fa4c
LM <emptymalei@qq.com>
2020-03-27 11:57:32 +0100

    download AT from dashboard

--- scripts/download_at.py
+++ scripts/download_at.py
@@ -3,6 +3,7 @@ import logging
 import os
 import re
 from functools import reduce
+import json
 
 import dateutil
 import lxml
@@ -11,24 +12,29 @@ import requests
 from lxml import etree
 
 from utils import (_COLUMNS_ORDER, COVIDScrapper, DailyAggregator,
-                   DailyTransformation, retrieve_files)
+                   DailyTransformation, retrieve_files, get_response)
 
 logging.basicConfig()
 logger = logging.getLogger("covid-eu-data.download.at")
 
+AT_BUNDESLAND_URL = "https://info.gesundheitsministerium.at/data/Bundesland.js"
+AT_TOTAL_URL = "https://info.gesundheitsministerium.at/data/SimpleData.js"
 AT_REPORT_URL = "https://www.sozialministerium.at/Informationen-zum-Coronavirus/Neuartiges-Coronavirus-(2019-nCov).html"
+
+HOSPITALIZED = "https://www.sozialministerium.at/Informationen-zum-Coronavirus/Dashboard/Zahlen-zur-Hospitalisierung"
 DAILY_FOLDER = os.path.join("dataset", "daily", "at")
-AT_STATES = [
-    "Burgenland",
-    "Kärnten",
-    "Niederösterreich",
-    "Oberösterreich",
-    "Salzburg",
-    "Steiermark",
-    "Tirol",
-    "Vorarlberg",
-    "Wien"
-]
+CACHE_FOLDER = os.path.join("cache", "daily", "at")
+AT_STATES = {
+    "Bgld": "Burgenland",
+    "Ktn": "Kärnten",
+    "NÖ": "Niederösterreich",
+    "OÖ": "Oberösterreich",
+    "Sbg": "Salzburg",
+    "Stmk": "Steiermark",
+    "T": "Tirol",
+    "Vbg": "Vorarlberg",
+    "W": "Wien"
+}
 
 class SARSCOV2AT(COVIDScrapper):
     def __init__(self, url=None, daily_folder=None):
@@ -46,71 +52,43 @@ class SARSCOV2AT(COVIDScrapper):
 
         geo_loc_key = 'nuts_2'
 
-        doc = lxml.html.document_fromstring(self.req.text)
-        el = doc.xpath('.//div[@class="infobox"]')
-        if el:
-            paragraphs = [
-                "".join(i.xpath('.//text()')) for i in el[0].xpath('.//p')
-            ]
-            cases_text = ""
-            recovered_text = ""
-            deaths_text = ""
-            for par in paragraphs:
-                if par.startswith("Bestätigte Fälle, "):
-                    cases_text = par
-                    cases_text = html.unescape(cases_text)
-                elif par.startswith("Genesene Personen, "):
-                    recovered_text = par
-                    recovered_text = html.unescape(recovered_text)
-                elif par.startswith("Todesfälle, "):
-                    deaths_text = par
-                    deaths_text = html.unescape(deaths_text)
-        else:
-            raise Exception("Could not find infobox")
+        hos_df = pd.read_html(HOSPITALIZED)
+        if not hos_df:
+            raise Exception("Could not find hospitalized table")
+        hos_df = hos_df[0]
+        hos_df.rename(
+            columns = {
+                "Bundesland": geo_loc_key,
+                "Hospitalisierung": "hospitalized",
+                "Intensivstation": "intensive_case"
+            },
+            inplace=True
+        )
+        hos_df.replace("Österreich gesamt", "", inplace=True)
 
-        re_cases = re.compile(r'\s(\w*?)\s\((\d+)\)')
-        re_deaths = re.compile(r'\s(\d+)\s\((\w+)\)')
-
-        cases = [i for i in re_cases.findall(cases_text) if i[0] in AT_STATES]
-        cases = [(s, v.replace('.','').replace(',','.')) for s,v in cases]
-        recovered = [
-            i for i in re_cases.findall(recovered_text) if i[0] in AT_STATES
+        cases_req = get_response(AT_BUNDESLAND_URL)
+        re_cases = re.compile(r'var dpBundesland = (\[.*\]);')
+        cases = re_cases.findall(cases_req.text)[0]
+        cases = json.loads(cases)
+        cases = [
+            {geo_loc_key: AT_STATES[i["label"]], "cases": int(i["y"])}
+            for i in cases
         ]
-        recovered = [(s, v.replace('.','').replace(',','.')) for s,v in recovered]
-        deaths = [i for i in re_deaths.findall(deaths_text) if i[-1] in AT_STATES]
-        deaths = [(v.replace('.','').replace(',','.'), s) for v, s in deaths]
+        cases_total_req = get_response(AT_TOTAL_URL)
+        re_cases_total = re.compile(r'var Erkrankungen = (\d+);')
+        cases_total = re_cases_total.findall(cases_total_req.text)[0]
+        cases_total = {geo_loc_key: "", "cases": cases_total}
+        cases.append(cases_total)
+        df_cases = pd.DataFrame(cases)
 
-        if not cases:
-            raise Exception("Could not find cases_text in webpage")
-
-        df_cases = pd.DataFrame(
-            cases, columns=[geo_loc_key, "cases"]
+        self.df = pd.merge(
+            df_cases, hos_df, how="outer",
+            on=geo_loc_key
         )
-
-        df_recovered = pd.DataFrame(
-            recovered, columns=[geo_loc_key, "recovered"]
-        )
-        df_deaths = pd.DataFrame(
-            deaths, columns=["deaths", geo_loc_key]
-        )
-        self.df = reduce(
-            lambda  left,right: pd.merge(
-                left,right,on=[geo_loc_key], how='outer'
-            ), [df_cases, df_recovered, df_deaths]
-        )
-        self.df.fillna(0, inplace=True)
+        self.df.fillna("", inplace=True)
         self.df["cases"] = self.df.cases.astype(int)
-        self.df["recovered"] = self.df.recovered.astype(int)
-        self.df["deaths"] = self.df.deaths.astype(int)
-
-        # total = self.df[["cases", "recovered", "deaths"]].sum()
-        # total["state"] = "sum"
-
-        # self.df = self.df.append(
-        #     pd.DataFrame(
-        #         total
-        #     ).T
-        # )
+        # self.df["hospitalized"] = self.df.hospitalized.astype(int)
+        # self.df["intensive_case"] = self.df.intensive_case.astype(int)
 
         logger.info("cases:\n", self.df)
 
@@ -118,16 +96,12 @@ class SARSCOV2AT(COVIDScrapper):
         """Get datetime of dataset
         Aktuelle Situation Österreich 04.03.2020 / 17:45 Uhr
         Stand, 10.03.2020, 08:00 Uhr
+        Stand 27.03.2020, 08:00 Uhr
         """
-        doc = lxml.html.document_fromstring(self.req.text)
-        el = doc.xpath('.//div[@class="infobox"]')
-        if el:
-            text = "".join(
-                el[0].xpath('.//text()')
-            )
+        req = get_response(HOSPITALIZED)
 
-        re_dt = re.compile(r'Bestätigte Fälle, Stand (\d{1,2}.\d{1,2}.\d{4}, \d{1,2}:\d{1,2}) Uhr:')
-        text = html.unescape(text)
+        re_dt = re.compile(r'Stand.*(\d{1,2}.\d{1,2}.\d{4}, \d{1,2}:\d{1,2}) Uhr')
+        text = html.unescape(req.content.decode(req.apparent_encoding))
         dt_from_re = re_dt.findall(text)
 
         if not dt_from_re:
@@ -142,26 +116,34 @@ class SARSCOV2AT(COVIDScrapper):
         self.df.sort_values(by="cases", inplace=True)
 
 
+def cache_content(url, dt, name):
+    req = get_response(url)
+    with open(
+            os.path.join(CACHE_FOLDER, f"{dt}__{name}"),
+            'wb'
+        ) as f:
+            f.write(req.content)
+
 if __name__ == "__main__":
 
-    column_converter = {
-        "state": "nuts_2"
-    }
-    drop_rows = {
-        "state": "sum"
-    }
+    # column_converter = {
+    #     "state": "nuts_2"
+    # }
+    # drop_rows = {
+    #     "state": "sum"
+    # }
 
-    daily_files = retrieve_files(DAILY_FOLDER)
-    daily_files.sort()
+    # daily_files = retrieve_files(DAILY_FOLDER)
+    # daily_files.sort()
 
-    for file in daily_files:
-        file_path = os.path.join(DAILY_FOLDER, file)
-        file_transformation = DailyTransformation(
-            file_path=file_path,
-            column_converter=column_converter,
-            drop_rows=drop_rows
-        )
-        file_transformation.workflow()
+    # for file in daily_files:
+    #     file_path = os.path.join(DAILY_FOLDER, file)
+    #     file_transformation = DailyTransformation(
+    #         file_path=file_path,
+    #         column_converter=column_converter,
+    #         drop_rows=drop_rows
+    #     )
+    #     file_transformation.workflow()
 
     cov_at = SARSCOV2AT()
     cov_at.workflow()
@@ -175,5 +157,15 @@ if __name__ == "__main__":
         fill=False
     )
     da.workflow()
+
+    to_be_cached ={
+        "Bezirke": "https://info.gesundheitsministerium.at/data/Bezirke.js",
+        "Geschlechtsverteilung": "https://info.gesundheitsministerium.at/data/Geschlechtsverteilung.js",
+        "Altersverteilung": "https://info.gesundheitsministerium.at/data/Altersverteilung.js",
+        "SimpleData": "https://info.gesundheitsministerium.at/data/SimpleData.js",
+        "page": "https://www.sozialministerium.at/Informationen-zum-Coronavirus/Neuartiges-Coronavirus-(2019-nCov).html"
+    }
+    for key,val in to_be_cached.items():
+        cache_content(val, cov_at.datetime, key)
 
     print("End of Game")
