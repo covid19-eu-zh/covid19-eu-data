7d085ac5b91648aa332c9cfadf54bad52ea10172
GitHub Action <action@github.com>
2020-04-01 15:13:33 +0000

    Update AT Dataset

--- cache/daily/at/2020-04-01T10:00:00__Altersverteilung.js
+++ cache/daily/at/2020-04-01T10:00:00__Altersverteilung.js
@@ -1,2 +1,2 @@
-var dpAltersverteilung = [{"label":"<5","y":43},{"label":"5-14","y":226},{"label":"15-24","y":967},{"label":"25-34","y":1526},{"label":"35-44","y":1541},{"label":"45-54","y":2229},{"label":"55-64","y":1816},{"label":">64","y":2153}];
-var AltersverteilungVersion = "01.04.2020 16:00:00 V 1.1.0.0"
+var dpAltersverteilung = [{"label":"<5","y":44},{"label":"5-14","y":228},{"label":"15-24","y":970},{"label":"25-34","y":1534},{"label":"35-44","y":1548},{"label":"45-54","y":2242},{"label":"55-64","y":1825},{"label":">64","y":2162}];
+var AltersverteilungVersion = "01.04.2020 17:00:00 V 1.1.0.0"
--- cache/daily/at/2020-04-01T10:00:00__SimpleData.js
+++ cache/daily/at/2020-04-01T10:00:00__SimpleData.js
@@ -1,3 +1,3 @@
-var Erkrankungen = "10501";
-var LetzteAktualisierung = "01.04.2020 16:00.00";
-var SimpleDataVersion = "01.04.2020 16:00:00 V 1.1.0.0"
+var Erkrankungen = "10553";
+var LetzteAktualisierung = "01.04.2020 17:00.00";
+var SimpleDataVersion = "01.04.2020 17:00:00 V 1.1.0.0"
