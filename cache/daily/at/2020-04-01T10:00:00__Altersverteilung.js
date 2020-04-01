eae39c1ef1314555065d0751be0d4bfbb263496f
GitHub Action <action@github.com>
2020-04-01 16:13:42 +0000

    Update AT Dataset

--- cache/daily/at/2020-04-01T10:00:00__Altersverteilung.js
+++ cache/daily/at/2020-04-01T10:00:00__Altersverteilung.js
@@ -1,2 +1,2 @@
-var dpAltersverteilung = [{"label":"<5","y":44},{"label":"5-14","y":228},{"label":"15-24","y":970},{"label":"25-34","y":1534},{"label":"35-44","y":1548},{"label":"45-54","y":2242},{"label":"55-64","y":1825},{"label":">64","y":2162}];
-var AltersverteilungVersion = "01.04.2020 17:00:00 V 1.1.0.0"
+var dpAltersverteilung = [{"label":"<5","y":44},{"label":"5-14","y":228},{"label":"15-24","y":973},{"label":"25-34","y":1538},{"label":"35-44","y":1555},{"label":"45-54","y":2249},{"label":"55-64","y":1828},{"label":">64","y":2170}];
+var AltersverteilungVersion = "01.04.2020 18:00:00 V 1.1.0.0"
