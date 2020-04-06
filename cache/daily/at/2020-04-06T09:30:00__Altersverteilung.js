62d25d3a9859b1f14d5ecb859521c46e55a5b9b6
GitHub Action <action@github.com>
2020-04-06 11:13:37 +0000

    Update AT Dataset

--- cache/daily/at/2020-04-06T09:30:00__Altersverteilung.js
+++ cache/daily/at/2020-04-06T09:30:00__Altersverteilung.js
@@ -1,2 +1,2 @@
-var dpAltersverteilung = [{"label":"<5","y":54},{"label":"5-14","y":280},{"label":"15-24","y":1122},{"label":"25-34","y":1736},{"label":"35-44","y":1748},{"label":"45-54","y":2576},{"label":"55-64","y":2052},{"label":">64","y":2515}];
-var AltersverteilungVersion = "06.04.2020 12:00:00 V 1.1.0.0"
+var dpAltersverteilung = [{"label":"<5","y":54},{"label":"5-14","y":280},{"label":"15-24","y":1122},{"label":"25-34","y":1739},{"label":"35-44","y":1748},{"label":"45-54","y":2579},{"label":"55-64","y":2056},{"label":">64","y":2531}];
+var AltersverteilungVersion = "06.04.2020 13:00:00 V 1.1.0.0"
--- cache/daily/at/2020-04-06T09:30:00__Geschlechtsverteilung.js
+++ cache/daily/at/2020-04-06T09:30:00__Geschlechtsverteilung.js
@@ -1,2 +1,2 @@
 var dpGeschlechtsverteilung = [{"label":"männlich","y":51},{"label":"weiblich","y":49}];
-var GeschlechtsverteilungVersion = "06.04.2020 12:00:00 V 1.1.0.0"
+var GeschlechtsverteilungVersion = "06.04.2020 13:00:00 V 1.1.0.0"
