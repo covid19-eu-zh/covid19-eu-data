82814bc9ead882535ade55bd65bcec35e89ca82b
GitHub Action <action@github.com>
2020-04-14 09:14:16 +0000

    Update AT Dataset

---
+++ cache/daily/at/2020-04-14T09:30:00__Altersverteilung.js
@@ -0,0 +1,2 @@
+var dpAltersverteilung = [{"label":"<5","y":71},{"label":"5-14","y":315},{"label":"15-24","y":1323},{"label":"25-34","y":1959},{"label":"35-44","y":1978},{"label":"45-54","y":2990},{"label":"55-64","y":2385},{"label":">64","y":3085}];
+var AltersverteilungVersion = "14.04.2020 11:00:00 V 1.1.0.0"
---
+++ cache/daily/at/2020-04-14T09:30:00__SimpleData.js
@@ -0,0 +1,3 @@
+var Erkrankungen = "14106";
+var LetzteAktualisierung = "14.04.2020 11:00.00";
+var SimpleDataVersion = "14.04.2020 11:00:00 V 1.1.0.0"
