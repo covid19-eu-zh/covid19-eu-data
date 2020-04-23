61468b60ab2a0d9efd755538b03291ea8a487122
LM <emptymalei@qq.com>
2020-04-23 21:41:02 +0200

    SI: downloading full dataset

--- scripts/download_si.py
+++ scripts/download_si.py
@@ -13,6 +13,7 @@ from utils import (_COLUMNS_ORDER, COVIDScrapper, DailyAggregator,
 logging.basicConfig()
 logger = logging.getLogger("covid-eu-data.download.si")
 
+XLSX_DATA_URL = "https://www.gov.si/assets/vlada/Koronavirus-podatki/en/EN_Covid-19-all-data.xlsx"
 REPORT_URL = "https://www.gov.si/en/topics/coronavirus-disease-covid-19/"
 DAILY_FOLDER = os.path.join("dataset", "daily", "si")
 CACHE_FOLDER = os.path.join("cache", "daily", "si")
@@ -27,7 +28,7 @@ class SARSCOV2SI(COVIDScrapper):
 
         COVIDScrapper.__init__(self, url, country="SI", daily_folder=daily_folder)
 
-    def extract_table(self):
+    def _extract_table_from_webpage(self):
         """Load data table from web page
         """
         req_dfs = pd.read_html(self.req.content, flavor='lxml')
@@ -84,7 +85,45 @@ def cache_table():
             f.write(get_response(REPORT_URL).content)
 
 
+def download_and_xlsx(xlsx_url):
+
+
+    with open(
+            os.path.join(CACHE_FOLDER, f"full_data.xlsx"),
+            'wb'
+        ) as f:
+            f.write(get_response(xlsx_url).content)
+
+    df = pd.read_excel(xlsx_url)
+
+    cols = {
+        "Date": "datetime",
+        "Tested (all)": "tests",
+        "Positive (all)": "tests_positive",
+        "All hospitalized on certain day": "hospitalized",
+        "All persons in intensive care on certain day": "intensive_care",
+        "Deaths (all)": "deaths"
+    }
+
+    df = df[list(cols.keys())]
+    df.rename(columns=cols, inplace=True)
+    df["cases"] = df.tests_positive
+    df = df[[i for i in _COLUMNS_ORDER if i in df.columns]]
+    df["datetime"] = df.datetime.apply(
+        lambda x: x.isoformat()
+    )
+
+    df.sort_values(by=["datetime", "cases"], inplace=True)
+
+    full_csv = os.path.join("dataset", "covid-19-si.csv")
+    df.to_csv(
+        full_csv, index=False
+    )
+
+
 if __name__ == "__main__":
+
+    download_and_xlsx(XLSX_DATA_URL)
 
     cache_table()
 
