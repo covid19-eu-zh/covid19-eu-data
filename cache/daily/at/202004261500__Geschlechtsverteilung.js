6cb0ad9d79a7c4278aed6812ea860445f2780996
GitHub Action <action@github.com>
2020-04-26 16:14:23 +0000

    Update AT Dataset

--- cache/daily/at/202004261500__Geschlechtsverteilung.js
+++ cache/daily/at/202004261500__Geschlechtsverteilung.js
@@ -1,2 +1,2 @@
 var dpGeschlechtsverteilung = [{"label":"weiblich","y":51,"CSV":"weiblich;51"},{"label":"männlich","y":49,"CSV":"männlich;49"}];
-var GeschlechtsverteilungVersion = "26.04.2020 17:00:00 V 1.2.2.0"
+var GeschlechtsverteilungVersion = "26.04.2020 18:00:00 V 1.2.2.0"
