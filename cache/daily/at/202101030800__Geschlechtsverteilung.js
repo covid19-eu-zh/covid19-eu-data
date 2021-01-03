0c23598e09182e613050948a9e514bf2cb726d89
LM <hi@leima.is>
2021-01-03 10:22:58 +0100

    test at

---
+++ cache/daily/at/202101030800__Geschlechtsverteilung.js
@@ -0,0 +1,2 @@
+var dpGeschlechtsverteilung = [{"label":"weiblich","y":48,"CSV":"weiblich;48"},{"label":"männlich","y":52,"CSV":"männlich;52"}];
+var GeschlechtsverteilungVersion = "07.10.2020 11:00:00 V 1.2.3.0"
---
+++ cache/daily/at/202101030800__SimpleData.js
@@ -0,0 +1,3 @@
+var Erkrankungen = "51230";
+var LetzteAktualisierung = "07.10.2020 11:00:00";
+var SimpleDataVersion = "07.10.2020 11:00:00 V 1.2.3.0"
