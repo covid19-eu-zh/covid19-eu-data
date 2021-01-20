fd1ee144914d4429ec7775a79c5c293ba8392fb1
LM <hi@leima.is>
2021-01-20 14:27:22 +0000

    fix pl

--- scripts/download_pl.py
+++ scripts/download_pl.py
@@ -3,6 +3,7 @@ import json
 import logging
 import os
 import re
+import io
 
 import dateutil
 import pandas as pd
@@ -10,25 +11,29 @@ import requests
 from lxml import etree, html
 
 from utils import (_COLUMNS_ORDER, COVIDScrapper, DailyAggregator,
-                   DailyTransformation, retrieve_files)
+                   DailyTransformation, retrieve_files, get_response)
 
 logging.basicConfig()
 logger = logging.getLogger("covid-eu-data.download.pl")
 
-REPORT_URL = "https://www.gov.pl/web/koronawirus/wykaz-zarazen-koronawirusem-sars-cov-2"
+# REPORT_URL = "https://www.gov.pl/web/koronawirus/wykaz-zarazen-koronawirusem-sars-cov-2"
+REPORT_URL = "https://covid19-rcb-info.hub.arcgis.com/"
+DATA_DATE_URL = "https://services9.arcgis.com/RykcEgwHWuMsJXPj/arcgis/rest/services/global_corona_actual_widok2/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&resultOffset=0&resultRecordCount=1&resultType=standard&cacheHint=true"
+CSV_DATA_URL = "https://arcgis.com/sharing/rest/content/items/829ec9ff36bc45a88e1245a82fff4ee0/data"
 DAILY_FOLDER = os.path.join("dataset", "daily", "pl")
+
 
 class SARSCOV2PL(COVIDScrapper):
     def __init__(self, url=None, daily_folder=None):
         if url is None:
-            url = REPORT_URL
+            url = CSV_DATA_URL
 
         if daily_folder is None:
             daily_folder = DAILY_FOLDER
 
         COVIDScrapper.__init__(self, url, country="PL", daily_folder=daily_folder)
 
-    def extract_table(self):
+    def extract_table_html(self):
         """Load data table from web page
         """
         doc = html.document_fromstring(self.req.content.decode('utf-8'))
@@ -68,12 +73,59 @@ class SARSCOV2PL(COVIDScrapper):
 
         logger.info("cases:\n", self.df)
 
+    def extract_table(self):
+        """Load data table from web page
+        """
+
+        df = pd.read_csv(io.StringIO(self.req.text), sep=';')
+
+        if df.empty:
+            raise Exception("Did not find data on webpage")
+
+        self.df = df # Województwo; Liczba; Liczba zgonów; Id
+        col_map = {
+                "wojewodztwo": "nuts_2",
+                "liczba_przypadkow": "cases",
+                "liczba_na_10_tys_mieszkancow": "cases/100k pop.",
+                "zgony": "deaths",
+                "liczba_wykonanych_testow": "tests",
+                "liczba_testow_z_wynikiem_pozytywnym": "tests_positive",
+                "liczba_osob_objetych_kwarantanna": "quarantine"
+            }
+        self.df.rename(
+            columns=col_map,
+            inplace=True
+        )
+        self.df.replace("Cała Polska", "sum", inplace=True)
+        self.df.replace("Cały kraj", "sum", inplace=True)
+        self.df.replace("Ca³y kraj", "sum", inplace=True)
+
+        self.df.drop(
+            self.df.loc[
+                self.df['nuts_2'] == 'sum'
+            ].index,
+            inplace=True
+        )
+
+        # Remove space from case numbers
+        self.df['cases'] = self.df['cases'].astype(str).str.replace(' ', '')
+        self.df = self.df[set(col_map.values()).intersection(set(self.df.columns))]
+
+        logger.info("cases:\n", self.df)
+
+
     def extract_datetime(self):
         """Get datetime of dataset
         """
-        current_date = datetime.datetime.now()
+        try:
+            req = get_response(DATA_DATE_URL)
+        except Exception as e:
+            raise Exception(e)
+
+        data = req.json().get('features')[0].get('attributes').get('DATA_SHOW')
+        dt = pd.to_datetime(data)
         self.dt = datetime.datetime(
-            current_date.year, current_date.month, current_date.day
+            dt.year, dt.month, dt.day
         )
 
     def post_processing(self):
