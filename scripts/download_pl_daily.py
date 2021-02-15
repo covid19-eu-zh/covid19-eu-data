86e476bc9cfb5b01a9d4d174b7d33fabe8969802
LM <hi@leima.is>
2021-02-15 18:14:51 +0000

    download monthly PL data and aggregate

--- scripts/download_pl_daily.py
+++ scripts/download_pl_daily.py
@@ -4,7 +4,6 @@ import logging
 import os
 import re
 import io
-import zipfile
 
 import dateutil
 import pandas as pd
@@ -23,15 +22,7 @@ REPORT_URL = "https://covid19-rcb-info.hub.arcgis.com/"
 DATA_DATE_URL = "https://services9.arcgis.com/RykcEgwHWuMsJXPj/arcgis/rest/services/global_corona_actual_widok2/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&resultOffset=0&resultRecordCount=1&resultType=standard&cacheHint=true"
 # CSV_DATA_URL = "https://arcgis.com/sharing/rest/content/items/829ec9ff36bc45a88e1245a82fff4ee0/data"
 CSV_DATA_URL = "https://arcgis.com/sharing/rest/content/items/153a138859bb4c418156642b5b74925b/data"
-MONTHLY_ARCHIVE_URL = "https://arcgis.com/sharing/rest/content/items/a8c562ead9c54e13a135b02e0d875ffb/data"
 DAILY_FOLDER = os.path.join("dataset", "daily", "pl")
-
-
-def download_url(url, save_path, chunk_size=128):
-    r = requests.get(url, stream=True)
-    with open(save_path, 'wb') as fd:
-        for chunk in r.iter_content(chunk_size=chunk_size):
-            fd.write(chunk)
 
 
 class SARSCOV2PL(COVIDScrapper):
@@ -43,9 +34,6 @@ class SARSCOV2PL(COVIDScrapper):
             daily_folder = DAILY_FOLDER
 
         COVIDScrapper.__init__(self, url, country="PL", daily_folder=daily_folder)
-
-    def _get_req(self):
-        pass
 
     def extract_table_html(self):
         """Load data table from web page
@@ -91,7 +79,7 @@ class SARSCOV2PL(COVIDScrapper):
         """Load data table from web page
         """
 
-        df = pd.read_csv(self.url, encoding="ISO-8859-1", sep=';')
+        df = pd.read_csv(io.StringIO(self.req.text), sep=';')
 
 
         if df.empty:
@@ -126,10 +114,10 @@ class SARSCOV2PL(COVIDScrapper):
         self.df['cases'] = self.df['cases'].astype(str).str.replace(' ', '')
         self.df = self.df[set(col_map.values()).intersection(set(self.df.columns))]
 
-        # if df['stan_rekordu_na'].unique():
-        #     self.dt = pd.to_datetime(df['stan_rekordu_na'].unique()[0])
-        # else:
-        #     raise ValueError('PL date column stan_rekordu_na has no value')
+        if df['stan_rekordu_na'].unique():
+            self.dt = pd.to_datetime(df['stan_rekordu_na'].unique()[0])
+        else:
+            raise ValueError('PL date column stan_rekordu_na has no value')
 
         logger.info("cases:\n", self.df)
 
@@ -137,10 +125,16 @@ class SARSCOV2PL(COVIDScrapper):
     def extract_datetime(self):
         """Get datetime of dataset
         """
+        # try:
+        #     req = get_response(DATA_DATE_URL)
+        # except Exception as e:
+        #     raise Exception(e)
 
-        # 20201224150439_rap_rcb_woj_eksport.csv
-        self.dt = pd.to_datetime(self.url.split("/")[-1][:8], dayfirst=False)
-
+        # data = req.json().get('features')[0].get('attributes').get('DATA_SHOW')
+        # dt = pd.to_datetime(data, dayfirst=True)
+        # self.dt = datetime.datetime(
+        #     dt.year, dt.month, dt.day
+        # )
         if not self.dt:
             raise Exception("self.dt is null")
 
@@ -191,32 +185,11 @@ if __name__ == "__main__":
     #         drop_rows=drop_rows
     #     )
     #     file_transformation.workflow()
-    CACHE_FOLDER = os.path.join("cache", "daily", "pl")
 
-    monthly_zip_path = os.path.join(CACHE_FOLDER, "monthly.zip")
+    cov_pl = SARSCOV2PL()
+    cov_pl.workflow()
 
-    download_url(
-        MONTHLY_ARCHIVE_URL,
-        monthly_zip_path
-    )
-
-    with zipfile.ZipFile(monthly_zip_path, 'r') as zip_ref:
-        zip_ref.extractall(CACHE_FOLDER)
-
-    daily_files = [
-        i for i in
-        sorted(os.listdir(CACHE_FOLDER), reverse=True)
-        if i.startswith("20")
-    ]
-
-    for p in daily_files[:3]:
-        p = os.path.join(CACHE_FOLDER, p)
-        logger.info(f"Looking at {p}")
-
-        cov_pl = SARSCOV2PL(url=p)
-        cov_pl.workflow()
-
-        print(cov_pl.df)
+    print(cov_pl.df)
 
     da = DailyAggregator(
         base_folder="dataset",
